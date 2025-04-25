43c7a06190191516f6ca84b1754005d78c8b0fcc
edbabayan <ebabayan86@gmail.com>
2025-04-25 15:21:42 +0400

    ADD: env_variable_file, tree_file_path, diagram_path

--- src/config.py
+++ src/config.py
@@ -3,6 +3,10 @@ from pathlib import Path
 class CFG:
     root = Path(__file__).parent.parent
 
+    env_variable_file = root / ".env"
+    tree_file_path = root / "confluence_page_tree.json"
+    diagram_path = root / "confluence_page_tree_diagram"
+
     HEADERS_TO_SPLIT_ON = [
         ("h1", "Section"),
         ("h2", "Subsection"),
