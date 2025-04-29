9e6c5a388cd34672ea693543cce81ed5523e4717
edbabayan <ebabayan86@gmail.com>
2025-04-29 14:37:18 +0400

    CHANGE: to take in all keys of the page

--- src/meta_miner/statement.py
+++ src/meta_miner/statement.py
@@ -4,9 +4,13 @@ from dataclasses import dataclass
 
 @dataclass
 class PageState:
+    id: str
+    title: str
     content: list[dict[str, Any]]
 
 
 @dataclass
 class Output:
+    id: str
     title: str
+    content: list[dict[str, Any]]
